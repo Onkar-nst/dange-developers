b874bd9f81e88146c7f3606a3e04b1096a6642c0
Onkar-nst <onkardange15@gmail.com>
2025-03-24 03:52:43 +0530

    minor update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,7 +2,7 @@
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import ContactForm from "@/components/contactform";
+import ContactForm from "@/components/Contactform";
 import { Building, Phone, Mail } from "lucide-react";
 
 export default function ContactSection() {
